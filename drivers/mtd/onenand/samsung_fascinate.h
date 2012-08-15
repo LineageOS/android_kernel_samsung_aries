26c9f0b804f65acf2845c2aea3536d932f1a442f
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-08-15 20:24:09 +0700

    Use OneNAND for /data
    Writing on MoviNAND is very slow and so we use it on /system and use
    the space of /system for /data instead.
    
    Need corresponding modification on device tree
    
    Change-Id: Ife77bcda275970044a5abd448a6e853807d2a85d

--- drivers/mtd/onenand/samsung_fascinate.h
+++ drivers/mtd/onenand/samsung_fascinate.h
@@ -40,14 +40,14 @@ struct mtd_partition s3c_partition_info[] = {
 		.size		= (30*SZ_256K), //131
 	},
 	{	
-		.name		= "system",
+		.name		= "userdata",
 		.offset		= (132*SZ_256K),
-		.size		= (1400*SZ_256K), //1531
+		.size		= (1688*SZ_256K), //1819
 	},
 	{
 		.name		= "cache",
-		.offset		= (1532*SZ_256K),
-		.size		= (472*SZ_256K), //2003
+		.offset		= (1820*SZ_256K),
+		.size		= (184*SZ_256K), //2003
 	},
 	{       /* The reservoir area is used by Samsung's Block Management Layer (BML)
 	           to map good blocks from this reservoir to bad blocks in user
