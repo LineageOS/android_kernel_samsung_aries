3d1cdfe06ece3402c362b870365f558b158afef6
Humberto Borba <humberos@gmail.com>
2013-07-24 10:31:36 -0300

    add p1c support
    Change-Id: I3fe4552d50dca80e717436ac0a2aceb95ec8a5c8

--- arch/arm/mach-s5pv210/setup-sdhci-gpio.c
+++ arch/arm/mach-s5pv210/setup-sdhci-gpio.c
@@ -23,6 +23,12 @@
 #include <plat/regs-sdhci.h>
 #include <plat/sdhci.h>
 
+#ifdef CONFIG_MACH_P1
+#include <mach/gpio.h>
+#include <mach/gpio-p1.h>
+#endif
+
+
 #include "herring.h"
 
 #if defined (CONFIG_SAMSUNG_GALAXYS) || defined(CONFIG_SAMSUNG_FASCINATE)
@@ -61,7 +67,7 @@ void s5pv210_setup_sdhci0_cfg_gpio(struct platform_device *dev, int width)
 		printk(KERN_ERR "Wrong SD/MMC bus width : %d\n", width);
 	}
 
-	if (machine_is_herring() || machine_is_aries() || machine_is_p1()) {
+	if (machine_is_herring() || machine_is_aries()) {
 		s3c_gpio_cfgpin(S5PV210_GPJ2(7), S3C_GPIO_OUTPUT);
 		s3c_gpio_setpull(S5PV210_GPJ2(7), S3C_GPIO_PULL_NONE);
 		gpio_set_value(S5PV210_GPJ2(7), 1);
@@ -101,6 +107,11 @@ void s5pv210_setup_sdhci2_cfg_gpio(struct platform_device *dev, int width)
 {
 	unsigned int gpio;
 
+#ifdef CONFIG_PHONE_P1_CDMA
+	unsigned int memory_enable;
+	memory_enable = S5PV210_GPJ1(1);
+#endif
+
 	switch (width) {
 	/* Channel 2 supports 4 and 8-bit bus width */
 	case 8:
@@ -128,6 +139,13 @@ void s5pv210_setup_sdhci2_cfg_gpio(struct platform_device *dev, int width)
 	default:
 		printk(KERN_ERR "Wrong SD/MMC bus width : %d\n", width);
 	}
+
+#if defined(CONFIG_PHONE_P1_CDMA)
+	s3c_gpio_cfgpin(memory_enable, S3C_GPIO_OUTPUT);
+	s3c_gpio_setpull(memory_enable, S3C_GPIO_PULL_NONE);
+	gpio_set_value(memory_enable, 1);
+#endif
+
 }
 
 void s5pv210_setup_sdhci3_cfg_gpio(struct platform_device *dev, int width)
